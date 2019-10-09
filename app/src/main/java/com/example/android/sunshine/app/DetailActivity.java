e630a2e5d1bfbb4ebe69266ff26ca5524dd4888b
Nigel Bomett <nigelbomett@tingle.software>
2019-10-09 15:46:51 +0300

    add network code

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by Nigel on 6/15/2016.
+ */
+
+public class DetailActivity {
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by Nigel on 6/27/2016.
+ */
+
+public class SettingsActivity {
+}
